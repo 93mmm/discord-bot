48c0cca9e867ae8ac78f876a6dd8ff128a4bb215
Alexander Reshetnyak <nickolaev513@gmail.com>
2023-12-02 11:07:14 +0300

    helpers

--- src/db_holder.py
+++ src/db_holder.py
@@ -2,30 +2,8 @@ import sqlite3
 import traceback
 
 from src.const import UserData, SocCred
+from src.helpers import get_soc_rating_for_db, soc_rating_in_form
 
-
-def get_soc_rating_for_db(info: str) -> tuple[int, int]:
-    # ! receives string (possible cases: +inf, -inf, +number, -number)
-    # ! returns information that can be fitted into database 
-    # ! (social points: number, is_infinity (rating usage determinator))
-    social_points = 0
-    is_infinity = SocCred.GET_FROM_DB
-    if info.startswith("+inf"):
-        is_infinity = SocCred.P_INFINITY
-    elif info.startswith("-inf"):
-        is_infinity = SocCred.N_INFINITY
-    else:
-        social_points = int(info)
-    return social_points, is_infinity
-
-
-def soc_rating_in_form(social_points: str, is_infinity: str):
-    if is_infinity == SocCred.N_INFINITY:
-        return "-inf"
-    elif is_infinity == SocCred.P_INFINITY:
-        return "+inf"
-    return social_points
-    
 
 class Database:
     def __init__(self, filename: str):
---
+++ src/helpers.py
@@ -0,0 +1,24 @@
+from src.const import SocCred
+
+
+def get_soc_rating_for_db(info: str) -> tuple[int, int]:
+    # ! receives string (possible cases: +inf, -inf, +number, -number)
+    # ! returns information that can be fitted into database 
+    # ! (social points: number, is_infinity (rating usage determinator))
+    social_points = 0
+    is_infinity = SocCred.GET_FROM_DB
+    if info.startswith("+inf"):
+        is_infinity = SocCred.P_INFINITY
+    elif info.startswith("-inf"):
+        is_infinity = SocCred.N_INFINITY
+    else:
+        social_points = int(info)
+    return social_points, is_infinity
+
+
+def soc_rating_in_form(social_points: str, is_infinity: str):
+    if is_infinity == SocCred.N_INFINITY:
+        return "-inf"
+    elif is_infinity == SocCred.P_INFINITY:
+        return "+inf"
+    return social_points
